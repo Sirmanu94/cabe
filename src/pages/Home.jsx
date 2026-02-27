b34a5b0718f691ec1e37f70aca4ffa0f49c2d792
Emanuele <tuo_email@example.com>
2026-02-27 01:16:56 +0100

    p

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-import Hero from '../components/Hero';
+// import Hero from '../components/Hero'; // Rimuovi o commenta
+import HeroParallax from '../components/HeroParallax'; // <-- Importa il nuovo
 import AboutPreview from '../components/AboutPreview';
 import Servizi from '../components/Servizi';
 import Projects from '../components/Projects';
@@ -9,32 +10,29 @@ import Contact from '../components/Contact';
 const Home = () => {
   const location = useLocation();
 
-  // Questo useEffect scatta ogni volta che carichi la Home o cambia l'URL
   useEffect(() => {
     if (location.hash) {
-      // Se c'è un hash nell'URL (es. #servizi), toglie il #
       const id = location.hash.replace('#', '');
       const element = document.getElementById(id);
-      
-      // Mettiamo un piccolissimo ritardo per assicurarci che la pagina sia renderizzata
       if (element) {
         setTimeout(() => {
           element.scrollIntoView({ behavior: 'smooth' });
         }, 100);
       }
     } else {
-      // Se non c'è hash e sei appena tornato alla home, vai in cima
       window.scrollTo({ top: 0, behavior: 'smooth' });
     }
   }, [location]);
 
   return (
     <>
-      <div className="relative z-0">
-        <Hero />
-      </div>
+      {/* Sostituiamo il Canvas 3D e la vecchia Hero con il nuovo, ultra-wow parallasse cinematografico */}
+      <HeroParallax /> 
 
-      <div className="relative z-10 shadow-[0_-20px_50px_rgba(0,0,0,0.8)]"> 
+      {/* Questo div contiene il resto delle sezioni, scorre SOPRA la Hero
+          z-10 per assicurarci che il gradiente della Hero non lo copra e che scorra bene.
+          L'ombra è molto delicata per non tagliare lo spazio. */}
+      <div className="relative z-10 shadow- bg-cabe-bg"> 
         <AboutPreview />
         <Servizi />
         <Projects />
